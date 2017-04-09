abde9c2c6c4646b20a446568b2df75f2f1e90e1b
sxpiv <c14730631@mydit.ie>
2017-04-09 16:01:59 +0100

    Update script2-huser.sh

--- script2-huser.sh
+++ script2-huser.sh
@@ -1,4 +1,5 @@
-!/bin/bash
+#Lab5 - Task2
+#!/bin/bash
 echo "Hello, $USER. I wish to list some files of yours"
 echo "Listing files in the current directory, $PWD"
 ls # list files . Try some of the different ls options here.
