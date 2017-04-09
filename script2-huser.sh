987c77fb5365faa591999978370d447c281d03ef
sxpiv <c14730631@mydit.ie>
2017-04-09 14:18:03 +0100

    some_message

---
+++ script2-huser.sh
@@ -0,0 +1,4 @@
+!/bin/bash
+echo "Hello, $USER. I wish to list some files of yours"
+echo "Listing files in the current directory, $PWD"
+ls # list files . Try some of the different ls options here.
